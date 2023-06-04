a431195d7ef526e42a5e93f9ccb84c2e4d2b8b58
Bruno Marcelino <90558795+BrunoMarcelinoEustaquioAssis@users.noreply.github.com>
2023-06-04 20:26:24 -0300

    Git feat: 2
    Feat-Criação do construtor da classe

--- una-gqs-refatoracao-ex-02-202301-main/logo/Cliente.java
+++ una-gqs-refatoracao-ex-02-202301-main/logo/Cliente.java
@@ -1,5 +1,13 @@
 package logo;
-//Inclusão da nova classe cliente - git feat
+//Feat-Inclusão da nova classe cliente - git feat
+//Feat-Criação do contrutor da classe
 public class Cliente {
+    private int idade;
+    private String sexo;
+  
+    public Cliente(int idade, String sexo) {
+        this.idade = idade;
+        this.sexo = sexo;
+    }
 
 }
